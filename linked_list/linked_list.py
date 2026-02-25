58b787b235658528e590804b1a54946af158a794
Awni <awni.alquraini@gmail.com>
2026-02-24 20:36:35 -0800

    finalized testing and implemented delete at index

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -11,11 +11,11 @@ class LinkedList:
         return self.count
     
     def print(self):
-        print("Elements in LinkedList = Index: Data")
+        print("Elements in LinkedList: (Index: Data): ")
         temp = self.head
         num = 0
         while temp is not None:
-            print(num + ": " + temp.data)
+            print(f"{num}: {temp.data}")
             temp = temp.next
             num += 1
     
@@ -58,7 +58,7 @@ class LinkedList:
         if self.head.data == val:
             self.head = self.head.next
             self.count -= 1
-            print("Deleted val: " + val)
+            print(f"Deleted val: {val}")
             return
             
         current = self.head
@@ -66,9 +66,10 @@ class LinkedList:
             if current.next.data == val:
                 current.next = current.next.next
                 self.count -= 1
-                print("Deleted val: " + val)
+                print(f"Deleted val: {val}")
                 return
             current = current.next
+        print(f"No data of value {val} is in the Linked List")
     
     def addAtTail(self, val):
         self.insert(val)
@@ -95,6 +96,26 @@ class LinkedList:
         cur.next = new
         self.count += 1
     
+    def deleteAtIndex(self, idx):
+        if idx < 0 or idx >= self.count:
+            print("Index out of bounds")
+            return
+            
+        if idx == 0:
+            print(f"Deleted value: {self.head.data}")
+            self.head = self.head.next
+            self.count -= 1
+            return
+            
+        cur = self.head
+        i = 0
+        while i < (idx - 1):
+            cur = cur.next
+            i += 1
+        print(f"Deleted value: {cur.next.data}")
+        cur.next = cur.next.next
+        self.count -= 1
+    
     def max(self):
         if self.count == 0:
             print("Nothing in the Linked List")
@@ -105,7 +126,7 @@ class LinkedList:
             if temp.data > max:
                 max = temp.data
             temp = temp.next
-        print("Max value: " + max)
+        print(f"Max value: {max}")
     
     def min(self):
         if self.count == 0:
@@ -117,7 +138,7 @@ class LinkedList:
             if temp.data < min:
                 min = temp.data
             temp = temp.next
-        print("Min value: " + min)
+        print(f"Min value: {min}")
         
 def main():
     ll1 = LinkedList()
@@ -125,6 +146,25 @@ def main():
     ll1.addAtTail(2)
     ll1.addAtIndex(0, 3)
     ll1.print()
+    print(f"Size of ll1: {ll1.__len__()}")
+    ll1.delete(5)
+    ll1.delete(3)
+    ll1.print()
+    print(f"Size of ll1: {ll1.__len__()}")
+    for i in range (3, 101):
+        ll1.addAtTail(i)
+    ll1.print()
+    print(f"Size of ll1: {ll1.__len__()}")
+    ll1.deleteAtIndex(100)
+    ll1.max()
+    ll1.deleteAtIndex(99)
+    ll1.max()
+    print(f"Size of ll1: {ll1.__len__()}")
+    ll1.min()
+    ll1.deleteAtIndex(0)
+    print(f"Size of ll1: {ll1.__len__()}")
+    ll1.min()
+    ll1.print()
     
-if __name__ == "__main__:":
+if __name__ == "__main__":
     main()
