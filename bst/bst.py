7873e5b17a124221e50094c0eb8b07d33a67ec7d
Awni <awni.alquraini@gmail.com>
2026-03-01 22:57:50 -0800

    worked on bst for a bit. did abt half of the funcs

--- bst/bst.py
+++ bst/bst.py
@@ -9,6 +9,32 @@ class BinarySearchTree:
         self.root = root
         self.height = height
     
+    def pre_order_traversal(self, root):
+        if root is None:
+            return
+        print(f"{root.val}, ")
+        self.pre_order_traversal(root.left)
+        self.pre_order_traversal(root.right)
+        
+    def in_order_traversal(self, root):
+        if root is None:
+            return
+        self.in_order_traversal(root.left)
+        print(f"{root.val}, ")
+        self.in_order_traversal(root.right)
+        
+    def post_order_traversal(self, root):
+        if root is None:
+            return
+        self.post_order_traversal(root.left)
+        self.post_order_traversal(root.right)
+        print(f"{root.val}, ")
+        
     def tree_insert(self, val):
+        if self.root is None:
+            self.root = self.Node(val, None, None)
+        else:
+            if(val <= self.root.val):
+                self.root.left = self.Node(val, None, None)
         
             
