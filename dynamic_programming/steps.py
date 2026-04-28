795bfed201715e6ac0f1fb3887a221e219eb8f4a
Awni <awni.alquraini@gmail.com>
2026-04-28 15:46:30 -0700

    finished steps dynamic programing question

--- dynamic_programming/steps.py
+++ dynamic_programming/steps.py
@@ -5,25 +5,49 @@
 
 # basic recursive method
 def steps(n):
-    pass
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    return (steps(n-1) + steps(n-2) + steps(n-3))
 
 print(steps(4))
 
 
 # Steps (Dynamic, Memoized)
 def memoized_steps(n):
-    pass
+    memoized = [-1] * (n + 1)
+    memoized[0] = 0
+    memoized[1] = 1
+    memoized[2] = 2
+    memoized[3] = 4
+    return memoized_steps_helper(n, memoized)
+    
 
 
 # helper function
 def memoized_steps_helper(n, r):
-    pass
+    if r[n] != -1:
+        return r[n]
+    r[n] = memoized_steps_helper(n-1, r) + memoized_steps_helper(n-2, r) + memoized_steps_helper(n-3, r)
+    return r[n]
 
 print(memoized_steps(4))
 
 
 # Steps (Dynamic, Bottom Up)
 def bottom_up_steps(n):
-    pass
+    memoized = [0] * (n + 1)
+    memoized[0] = 0
+    memoized[1] = 1
+    memoized[2] = 2
+    memoized[3] = 4
+    for i in range(4, n + 1):
+        memoized[i] = memoized[i-1] + memoized[i-2] + memoized[i-3]
+    return memoized[n]
 
 print (bottom_up_steps(4))
